7114168d81680d421a602c09d969c8533a204b53
alexlcortes <jando.cortes@gmail.com>
2016-12-27 23:48:13 -0500

    form updates main name on page. does not allow empty string.

--- public/app.jsx
+++ public/app.jsx
@@ -1,29 +1,41 @@
 var Greeter = React.createClass({
-
+  // props get defined and these are the default props
   getDefaultProps() {
     return {
       name: 'React',
       message: 'Message goes here.'
     };
   },
-
+  // sets it's name state to the props value
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    };
+  },
   onButtonClick: function (e) {
     e.preventDefault();
 
-    var name = this.refs.name.value;
+    var nameRef = this.refs.name;
+    // once form is submited this.setState re-renders the compoments
+    var name = nameRef.value;
+    // this will clear the form field
+    nameRef.value = '';
 
-    alert(name);
+    if (typeof name === 'string' && name.length > 0){
+      this.setState ({
+        name: name
+      });
+    }
   },
-
+  // this fetches the value of name... and display to the screen
   render: function () {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name}!</h1>
         <p>{message}</p>
-
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button>Set name</button>
@@ -34,7 +46,7 @@ var Greeter = React.createClass({
 });
 
 var firstName = 'Jando';
-var firstMessage = 'This is from a React component.';
+var firstMessage = 'Please enter your name below...';
 
 ReactDOM.render(
   <Greeter name={firstName} message={firstMessage}/>,
