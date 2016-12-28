a7aeb36d86698fb93f25d434c77b38b1dc705644
alexlcortes <jando.cortes@gmail.com>
2016-12-27 23:11:19 -0500

    now I have two props...

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,31 @@
 var Greeter = React.createClass({
+
+  getDefaultProps() {
+    return {
+      name: 'React'
+    };
+    return {
+      message: 'Message'
+    };
+  },
+
   render: function () {
-    var  name = this.props.name;
+    var name = this.props.name;
+    var message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is from the component...</p>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Jando';
+var firstMessage = 'This is from a React component.';
+
 ReactDOM.render(
-  <Greeter name="Alejandro"/>,
+  <Greeter name={firstName} message={firstMessage}/>,
   document.getElementById('app')
 );
--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var app = express();
 app.use(express.static('public'));
 
 app.listen(3000, function() {
-  console.log('Port 3000 is running.');
+  console.log('Port 3000 as active.');
 });
