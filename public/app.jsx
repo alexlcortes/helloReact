f7e7812d33822cab8792f68e07c5388caaa23404
alexlcortes <jando.cortes@gmail.com>
2016-12-27 22:56:14 -0500

    added props to app.jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,17 @@
 var Greeter = React.createClass({
   render: function () {
+    var  name = this.props.name;
+
     return (
       <div>
-        <h1>Hello React. We are go!</h1>
+        <h1>Hello {name}!</h1>
         <p>This is from the component...</p>
-        <img src="http://img.cinemablend.com/cb/d/2/f/f/4/a/d2ff4a4b810264b39408e2c6d81b28b4f95225a1703be33db5ed9dbd27551930.jpg"/>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name="Alejandro"/>,
   document.getElementById('app')
 );
